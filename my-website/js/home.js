a931fa1bc69dc67b3dfdcf8a26f598e04a55eaa8
inztructor <rioske20p@gmail.com>
2025-06-30 16:27:49 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '43fb6e1a2a662fad6e58c69085a84e78';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
